5397158b7b3099ee76816caeef6c93307833cbac
Patipatkul <sc.patipatk@gmail.com>
2020-11-06 10:17:17 +0700

    commit 1

--- config/db.js
+++ config/db.js
@@ -2,7 +2,8 @@ var mongoose = require('mongoose');
 
 
 mongoose.connect('mongodb://localhost:27017/saveWork', {
-  useNewUrlParser: true
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
 mongoose.connection.on("connected", function() {
